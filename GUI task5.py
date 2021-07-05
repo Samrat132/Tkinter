daedb16ec599aaf671e3a566a14c7b28c42f6081
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-07-05 12:23:22 +0545

    GUI task5

--- GUI task5.py
+++ GUI task5.py
@@ -1,17 +1,17 @@
-from tinkter import*
+from tkinter import*
 
 dam = Tk()
 
 #creating a lable widget
 Label1 = Label(dam, text="Tkinter Program Beginning")
-Lable2 = Lable(dam, text="I am Samrat Rijal")
-Lable3 = Lable(dam, text="Have a good good")
+Label2 = Label(dam, text="I am Samrat Rijal")
+Label3 = Label(dam, text="Have a good good             ")
 
 #Shoving it onto the screen based upon x-axis and y-axis
 #that does not move having a constant position
-Lable1.grid(row=0, column=0)
-Lable2.grid(row=1, column=5)
-Lable3.grid(row=1, column=1)
+Label1.grid(row=0, column=0)
+Label2.grid(row=1, column=5)
+Label3.grid(row=1, column=1)
 
 
 mainloop()
